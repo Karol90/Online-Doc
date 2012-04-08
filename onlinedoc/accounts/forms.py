c6c1576c08ebda7b18723d49cffb384feb06cb3d
Kleko <kleko09@gmail.com>
2012-04-09 00:05:55 +0200

    Rejestracja chyba działa, wypada dodać email, oraz być może zmienić z zapisywania użytkownika do bazy na tworzenie za pomoca metody create_user or sth

--- onlinedoc/accounts/forms.py
+++ onlinedoc/accounts/forms.py
@@ -2,12 +2,16 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms.widgets import PasswordInput
+from django.forms.models import ModelForm
 
+from onlinedoc.accounts.models import Account
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Nazwa użytkownika')
     password = forms.CharField(widget=PasswordInput, label='Hasło')
         
-        
-    
+class RegisterForm(ModelForm):
+    class Meta:
+        model = Account
+        exclude = ('user',)
--- onlinedoc/accounts/views.py
+++ onlinedoc/accounts/views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.views import auth_login
+from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect
-from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 
-from onlinedoc.accounts.forms import LoginForm
+from onlinedoc.accounts.forms import LoginForm, RegisterForm
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.contrib.auth import authenticate, login
 
@@ -18,8 +18,8 @@ def acc(request):
 
 @csrf_exempt
 def login_view(request):
-    def errorHandle(error):
-        form = LoginForm()
+    def errorHandle(error, form):
+        #form = LoginForm()
         return render_to_response('accounts/login.html', {
                 'error' : error,
                 'form' : form,
@@ -40,16 +40,15 @@ def login_view(request):
                     login(request, user)
                     return redirect('home')
                 else:
-                    # Return a 'disabled account' error message
                     error = u'account disabled'
-                    return errorHandle(error)
+                    return errorHandle(error, form)
             else:
                  # Return an 'invalid login' error message.
                 error = u'invalid login'
-                return errorHandle(error)
+                return errorHandle(error, form)
         else:
             error = u'form is invalid'
-            return errorHandle(error)
+            return errorHandle(error, form)
     else:
         form = LoginForm() # An unbound form
         return render_to_response('accounts/login.html', {
@@ -66,15 +65,33 @@ def profile(request):
     user = request.user.get_profile()
     return render_to_response('accounts/profile.html', locals())
 
+@csrf_exempt
 def register(request):
-    print 'registration'
+    def errorHandle(error, user_form, account_form):
+        return render_to_response("accounts/register.html", {
+                                                             'error': error,
+                                                             'user_form': user_form,
+                                                             'account_form': account_form,
+                                                             })
+    
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            return HttpResponseRedirect("/books/")
+        user_form = UserCreationForm(request.POST)
+        account_form = RegisterForm(request.POST)
+        if user_form.is_valid() and account_form.is_valid():
+            new_user = user_form.save()
+            account = account_form.save(commit=False)
+            account.user = new_user
+            account.save()
+            return render_to_response("accounts/register_activation.html")
+        else:
+            error = u'form invalid'
+            return errorHandle(error, user_form, account_form)
     else:
-        form = UserCreationForm()
-    return render_to_response("accounts/register.html", {
-        'form': form,
-    })
+        user_form = UserCreationForm()
+        account_form = RegisterForm()
+        return render_to_response("accounts/register.html", {
+                                                             'user_form': user_form,
+                                                             'account_form': account_form,
+                                                             })
+
+
